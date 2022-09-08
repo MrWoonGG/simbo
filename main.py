5d380102f722e106db449ffa7569019fca7614a7
MrWoonWorldT1 <108611870+MrWoonWorldT1@users.noreply.github.com>
2022-09-08 14:35:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def on_message(message):
     for char in message.content:
         if char.isupper():
             i+=1
-    caps = '%.2f %%' % (100*(i/len(text))))
+    caps = '%.2f %%' % (100*(i/len(text)))
     if caps > len(message.content) / 2:
         if d == "0"
             message.channel.send("Пожалуйста, не используй так много капса!")
